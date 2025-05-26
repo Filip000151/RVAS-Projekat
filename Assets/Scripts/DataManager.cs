34cc60819ddd52996e83d51ba4d87b58f84b3ba0
sanjaprvulovic <sanja26prvulovic@gmail.com>
2025-05-26 19:27:43 +0200

    DbManager

---
+++ Assets/Scripts/DataManager.cs
@@ -0,0 +1,35 @@
+using UnityEngine;
+
+public class DataManager : MonoBehaviour
+{
+    #region singleton 
+        public static DataManager instance;
+    void Awake() {  instance = this; }
+
+    #endregion 
+
+    public string errorMessage { get; private set; }
+    public void ErrorMessage(string message)
+    {
+        errorMessage += message + "\n";
+    }
+    /// <summary>
+    /// Pozovi svaki put  pre konektovanja na bazu 
+    /// i posle svake konekcije.
+    /// </summary>
+    public void clearErrorMessage()
+    {
+        errorMessage = string.Empty;
+    }
+
+    public bool DbCheck ()
+    {
+        if (string.IsNullOrEmpty(errorMessage))
+        {
+            return true;
+        }
+        return false;
+    }
+
+
+}
