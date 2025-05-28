2f850f3040df16e21fc0ac31e432d9de04cc6a60
nevena03 <157527262+nevena03@users.noreply.github.com>
2025-05-28 17:14:42 +0200

    Spawnovanje figurica na pravilne pozicije

--- Assets/Scripts/Game/GameController.cs
+++ Assets/Scripts/Game/GameController.cs
@@ -3,9 +3,11 @@ using UnityEngine;
 public class GameController : MonoBehaviour
 {
     public GameObject ChessPiece;
-    private GameObject[,] positions = new GameObject[7, 7];
+    private GameObject[,] Positions = new GameObject[7, 7];
     private GameObject[] PlayerBlack = new GameObject[7];
     private GameObject[] PlayerWhite = new GameObject[7];
+
+    private string CurrentPlayer = "white";
 
 
     void Start()
@@ -20,6 +22,12 @@ public class GameController : MonoBehaviour
             Create("black_pawn",0,6),Create("black_pawn",1,6),Create("black_bishop",2,6),Create("black_king",3,6),Create("black_bishop",4,6),Create("black_pawn",5,6),Create("black_pawn",6,6)
         };
 
+        for(int i = 0; i<PlayerBlack.Length; i++)
+        {
+            SetPosition(PlayerBlack[i]);
+            SetPosition(PlayerWhite[i]);
+        }
+
 
 
 
@@ -28,12 +36,20 @@ public class GameController : MonoBehaviour
     public GameObject Create(string name, int x, int y)
     {
         GameObject obj = Instantiate(ChessPiece, new Vector3(0,0,-1), Quaternion.identity);
+        PieceController pc = obj.GetComponent<PieceController>();
+        pc.name = name;
+        pc.SetXBoard(x);
+        pc.SetYBoard(y);
+        pc.Activate();
         return obj;
     }
 
     public void SetPosition(GameObject obj)
     {
+        PieceController pc = obj.GetComponent<PieceController>();
+        Positions[pc.GetXBoard(), pc.GetYBoard()] = obj;
 
+        
     }
 
 
